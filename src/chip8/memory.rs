b3c699cc658c7709d7342ce4fba4469fcc5bb875
agabani <agabani@users.noreply.github.com>
2022-07-17 11:36:06 +0100

    refactor: split chip-8 memory

---
+++ src/chip8/memory.rs
@@ -0,0 +1,186 @@
+use std::io::{Cursor, Read, Write};
+
+use super::instruction::Instruction;
+
+pub(super) struct Memory {
+    /// 16-bit index register called "I" which is used to point at locations in memory
+    i: u16,
+    /// A program counter, often called just "PC", which points to the current instruction in memory
+    program_counter: u16,
+    /// Memory: CHIP-8 has direct access to up to 4 kilobytes of RAM
+    ram: Vec<u8>,
+    /// A stack for 16-bit addresses which is used to call subroutines/function and return from them
+    stack: Vec<u16>,
+    /// 16 8-bit general purpose variable registers numbered `0` through `F`, called `V0` through `VF`
+    v: [u8; 16],
+}
+
+impl Memory {
+    pub(super) fn new() -> Memory {
+        Memory {
+            i: 0,
+            program_counter: 0,
+            ram: vec![0; 4096],
+            stack: Vec::new(),
+            v: [0; 16],
+        }
+    }
+
+    pub(super) fn load_font(&mut self, font: &[u8]) -> crate::Result<()> {
+        let mut cursor = Cursor::new(&mut self.ram);
+        cursor.set_position(0x050);
+        cursor.write_all(font)?;
+        Ok(())
+    }
+
+    pub(super) fn load_rom(&mut self, rom: &[u8]) -> crate::Result<()> {
+        let mut cursor = Cursor::new(&mut self.ram);
+        cursor.set_position(0x200);
+        self.program_counter = 0x200;
+        cursor.write_all(rom)?;
+        Ok(())
+    }
+
+    pub(super) fn get_instruction(&mut self) -> Instruction {
+        let mut cursor = Cursor::new(&self.ram);
+        cursor.set_position(u64::from(self.program_counter));
+
+        let mut buf = [0; 2];
+        cursor.read_exact(&mut buf).unwrap();
+
+        let nibbles = [
+            (buf[0] & 0xF0) >> 4,
+            buf[0] & 0x0F,
+            (buf[1] & 0xF0) >> 4,
+            buf[1] & 0x0F,
+        ];
+
+        self.program_counter = cursor.position() as u16;
+
+        match nibbles {
+            [0x0, 0x0, 0xE, 0x0] => Instruction::ClearScreen,
+            [0x1, n2, n3, n4] => Instruction::Jump {
+                nnn: (u16::from(n2) << 8) + (u16::from(n3) << 4) + (u16::from(n4)),
+            },
+            [0x6, n2, n3, n4] => Instruction::SetRegister {
+                x: n2,
+                nn: (n3 << 4) + n4,
+            },
+            [0x7, n2, n3, n4] => Instruction::AddValueToRegister {
+                x: n2,
+                nn: (n3 << 4) + n4,
+            },
+            [0xA, n2, n3, n4] => Instruction::SetIndexRegister {
+                nnn: (u16::from(n2) << 8) + (u16::from(n3) << 4) + (u16::from(n4)),
+            },
+            [0xD, n2, n3, n4] => Instruction::DisplayDraw {
+                x: n2,
+                y: n3,
+                n: n4,
+            },
+            [n1, n2, n3, n4] => todo!("{:1X} {:1X} {:1X} {:1X}", n1, n2, n3, n4),
+        }
+    }
+
+    pub(super) fn set_program_counter(&mut self, program_counter: u16) {
+        self.program_counter = program_counter;
+    }
+
+    pub(super) fn get_v_register(&self, x: u8) -> u8 {
+        self.v[x as usize]
+    }
+
+    pub(super) fn set_v_register(&mut self, x: u8, nn: u8) {
+        self.v[x as usize] = nn;
+    }
+
+    pub(super) fn get_index_register(&self) -> u16 {
+        self.i
+    }
+
+    pub(super) fn set_index_register(&mut self, nnn: u16) {
+        self.i = nnn;
+    }
+
+    pub(super) fn get_ram_byte(&self, address: usize) -> u8 {
+        self.ram[address]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::io::Read;
+
+    use super::*;
+
+    #[test]
+    fn memory_get_instruction() {
+        let mut rom = Vec::new();
+        let mut file = std::fs::File::open("./roms/IBM Logo.ch8").unwrap();
+        file.read_to_end(&mut rom).unwrap();
+
+        let mut memory = Memory::new();
+        memory.load_rom(&rom).unwrap();
+
+        let instructions = vec![
+            // 00000000
+            Instruction::ClearScreen,
+            Instruction::SetIndexRegister { nnn: 0x22A },
+            Instruction::SetRegister { x: 0x0, nn: 0x0C },
+            Instruction::SetRegister { x: 0x1, nn: 0x08 },
+            Instruction::DisplayDraw {
+                x: 0x0,
+                y: 0x1,
+                n: 0xF,
+            },
+            Instruction::AddValueToRegister { x: 0x0, nn: 0x09 },
+            Instruction::SetIndexRegister { nnn: 0x239 },
+            Instruction::DisplayDraw {
+                x: 0x0,
+                y: 0x1,
+                n: 0xF,
+            },
+            // 00000010
+            Instruction::SetIndexRegister { nnn: 0x248 },
+            Instruction::AddValueToRegister { x: 0x0, nn: 0x08 },
+            Instruction::DisplayDraw {
+                x: 0x0,
+                y: 0x1,
+                n: 0xF,
+            },
+            Instruction::AddValueToRegister { x: 0x0, nn: 0x04 },
+            Instruction::SetIndexRegister { nnn: 0x257 },
+            Instruction::DisplayDraw {
+                x: 0x0,
+                y: 0x1,
+                n: 0xF,
+            },
+            Instruction::AddValueToRegister { x: 0x0, nn: 0x08 },
+            Instruction::SetIndexRegister { nnn: 0x266 },
+            // 00000020
+            Instruction::DisplayDraw {
+                x: 0x0,
+                y: 0x1,
+                n: 0xF,
+            },
+            Instruction::AddValueToRegister { x: 0x0, nn: 0x08 },
+            Instruction::SetIndexRegister { nnn: 0x275 },
+            Instruction::DisplayDraw {
+                x: 0x0,
+                y: 0x1,
+                n: 0xF,
+            },
+            Instruction::Jump { nnn: 0x228 },
+        ];
+
+        for (index, instruction) in instructions.into_iter().enumerate() {
+            assert_eq!(
+                memory.get_instruction(),
+                instruction,
+                "instruction {}",
+                index
+            );
+        }
+    }
+}
--- src/chip8/mod.rs
+++ src/chip8/mod.rs
@@ -1,30 +1,17 @@
-use std::io::{Cursor, Read, Write};
-
 mod display;
 mod font;
 mod instruction;
+mod memory;
 
 use display::Display;
 use font::Font;
 use instruction::Instruction;
+use memory::Memory;
 
 pub(crate) struct Emulator {
     display: Display,
     memory: Memory,
     paused: bool,
-}
-
-struct Memory {
-    /// 16-bit index register called "I" which is used to point at locations in memory
-    i: u16,
-    /// A program counter, often called just "PC", which points to the current instruction in memory
-    program_counter: u16,
-    /// Memory: CHIP-8 has direct access to up to 4 kilobytes of RAM
-    ram: Vec<u8>,
-    /// A stack for 16-bit addresses which is used to call subroutines/function and return from them
-    stack: Vec<u16>,
-    /// 16 8-bit general purpose variable registers numbered `0` through `F`, called `V0` through `VF`
-    v: [u8; 16],
 }
 
 impl Emulator {
@@ -59,23 +46,27 @@ impl Emulator {
 
         match self.memory.get_instruction() {
             Instruction::ClearScreen => self.display.clear_screen(),
-            Instruction::Jump { nnn } => self.memory.program_counter = nnn,
-            Instruction::SetRegister { x, nn } => self.memory.v[x as usize] = nn,
-            Instruction::AddValueToRegister { x, nn } => self.memory.v[x as usize] += nn,
-            Instruction::SetIndexRegister { nnn } => self.memory.i = nnn,
+            Instruction::Jump { nnn } => self.memory.set_program_counter(nnn),
+            Instruction::SetRegister { x, nn } => self.memory.set_v_register(x, nn),
+            Instruction::AddValueToRegister { x, nn } => self
+                .memory
+                .set_v_register(x, self.memory.get_v_register(x) + nn),
+            Instruction::SetIndexRegister { nnn } => self.memory.set_index_register(nnn),
             Instruction::DisplayDraw { x, y, n } => {
                 // Set the X coordinate to the value in VX modulo 64
-                let x = self.memory.v[x as usize] % 64;
+                let x = self.memory.get_v_register(x) % 64;
                 // Set the Y coordinate to the value in VY modulo 32
-                let y = self.memory.v[y as usize] % 32;
+                let y = self.memory.get_v_register(y) % 32;
 
                 // Set VF to 0
-                self.memory.v[0xF] = 0;
+                self.memory.set_v_register(0xF, 0);
 
                 // For N rows
                 for row in 0..n {
                     // Get the Nth byte of sprite data, counting from the memory address in the I register
-                    let sprite_data = self.memory.ram[self.memory.i as usize + row as usize];
+                    let sprite_data = self
+                        .memory
+                        .get_ram_byte(self.memory.get_index_register() as usize + row as usize);
 
                     // For each of the 8 pixels/bits in this sprite row
                     for pixel in 0..8 {
@@ -91,7 +82,7 @@ impl Emulator {
                             // turn off the pixel
                             self.display.set_pixel(x + pixel, y + n - row, false);
                             // set VF to 1
-                            self.memory.v[0xF] = 1;
+                            self.memory.set_v_register(0xF, 1);
                         }
                         // Or if the current pixel in the sprite row is on and the screen pixel is not
                         else if sprite_row_pixel && !display_pixel {
@@ -111,74 +102,6 @@ impl Emulator {
                     }
                 }
             }
-        }
-    }
-}
-
-impl Memory {
-    fn new() -> Memory {
-        Memory {
-            i: 0,
-            program_counter: 0,
-            ram: vec![0; 4096],
-            stack: Vec::new(),
-            v: [0; 16],
-        }
-    }
-
-    fn load_font(&mut self, font: &[u8]) -> crate::Result<()> {
-        let mut cursor = Cursor::new(&mut self.ram);
-        cursor.set_position(0x050);
-        cursor.write_all(font)?;
-        Ok(())
-    }
-
-    fn load_rom(&mut self, rom: &[u8]) -> crate::Result<()> {
-        let mut cursor = Cursor::new(&mut self.ram);
-        cursor.set_position(0x200);
-        self.program_counter = 0x200;
-        cursor.write_all(rom)?;
-        Ok(())
-    }
-
-    fn get_instruction(&mut self) -> Instruction {
-        let mut cursor = Cursor::new(&self.ram);
-        cursor.set_position(u64::from(self.program_counter));
-
-        let mut buf = [0; 2];
-        cursor.read_exact(&mut buf).unwrap();
-
-        let nibbles = [
-            (buf[0] & 0xF0) >> 4,
-            buf[0] & 0x0F,
-            (buf[1] & 0xF0) >> 4,
-            buf[1] & 0x0F,
-        ];
-
-        self.program_counter = cursor.position() as u16;
-
-        match nibbles {
-            [0x0, 0x0, 0xE, 0x0] => Instruction::ClearScreen,
-            [0x1, n2, n3, n4] => Instruction::Jump {
-                nnn: (u16::from(n2) << 8) + (u16::from(n3) << 4) + (u16::from(n4)),
-            },
-            [0x6, n2, n3, n4] => Instruction::SetRegister {
-                x: n2,
-                nn: (n3 << 4) + n4,
-            },
-            [0x7, n2, n3, n4] => Instruction::AddValueToRegister {
-                x: n2,
-                nn: (n3 << 4) + n4,
-            },
-            [0xA, n2, n3, n4] => Instruction::SetIndexRegister {
-                nnn: (u16::from(n2) << 8) + (u16::from(n3) << 4) + (u16::from(n4)),
-            },
-            [0xD, n2, n3, n4] => Instruction::DisplayDraw {
-                x: n2,
-                y: n3,
-                n: n4,
-            },
-            [n1, n2, n3, n4] => todo!("{:1X} {:1X} {:1X} {:1X}", n1, n2, n3, n4),
         }
     }
 }
@@ -208,75 +131,5 @@ mod tests {
 
         let mut emulator = Emulator::new();
         emulator.load_rom(&rom).unwrap();
-    }
-
-    #[test]
-    fn memory_get_instruction() {
-        let mut rom = Vec::new();
-        let mut file = std::fs::File::open("./roms/IBM Logo.ch8").unwrap();
-        file.read_to_end(&mut rom).unwrap();
-
-        let mut memory = Memory::new();
-        memory.load_rom(&rom).unwrap();
-
-        let instructions = vec![
-            // 00000000
-            Instruction::ClearScreen,
-            Instruction::SetIndexRegister { nnn: 0x22A },
-            Instruction::SetRegister { x: 0x0, nn: 0x0C },
-            Instruction::SetRegister { x: 0x1, nn: 0x08 },
-            Instruction::DisplayDraw {
-                x: 0x0,
-                y: 0x1,
-                n: 0xF,
-            },
-            Instruction::AddValueToRegister { x: 0x0, nn: 0x09 },
-            Instruction::SetIndexRegister { nnn: 0x239 },
-            Instruction::DisplayDraw {
-                x: 0x0,
-                y: 0x1,
-                n: 0xF,
-            },
-            // 00000010
-            Instruction::SetIndexRegister { nnn: 0x248 },
-            Instruction::AddValueToRegister { x: 0x0, nn: 0x08 },
-            Instruction::DisplayDraw {
-                x: 0x0,
-                y: 0x1,
-                n: 0xF,
-            },
-            Instruction::AddValueToRegister { x: 0x0, nn: 0x04 },
-            Instruction::SetIndexRegister { nnn: 0x257 },
-            Instruction::DisplayDraw {
-                x: 0x0,
-                y: 0x1,
-                n: 0xF,
-            },
-            Instruction::AddValueToRegister { x: 0x0, nn: 0x08 },
-            Instruction::SetIndexRegister { nnn: 0x266 },
-            // 00000020
-            Instruction::DisplayDraw {
-                x: 0x0,
-                y: 0x1,
-                n: 0xF,
-            },
-            Instruction::AddValueToRegister { x: 0x0, nn: 0x08 },
-            Instruction::SetIndexRegister { nnn: 0x275 },
-            Instruction::DisplayDraw {
-                x: 0x0,
-                y: 0x1,
-                n: 0xF,
-            },
-            Instruction::Jump { nnn: 0x228 },
-        ];
-
-        for (index, instruction) in instructions.into_iter().enumerate() {
-            assert_eq!(
-                memory.get_instruction(),
-                instruction,
-                "instruction {}",
-                index
-            );
-        }
     }
 }
